97db61875381ef09887b23c81b8fe4c60d3882f5
EcaCosca <enriquecoscarelli@gmail.com>
2022-12-02 17:25:14 +0100

    ex 4  start

--- index.js
+++ index.js
@@ -26,9 +26,12 @@ app.post('/api/countries', (req, res)=>{
 
 // 3. GET /api/countries/:code
 // This route should return a single country, based on the code provided. You should accept both an alpha 2 or an alpha 3 code. 
+app.get('/api/countries/:code', (req, res) => {
+    Country.findOne({$or: [{alpha2Code:req.params.code},{alpha3Code:req.params.code}]}, (err, data) => res.send(data));
+});
 
-
-
+// 4. PUT /api/countries/:code
+// This route should accept edits to an existing country in the list (eg: edit an object inside the countries array).
 
 app.listen(port, ()=>{
     console.log(`Server is running on http://localhost:${port}/`)
