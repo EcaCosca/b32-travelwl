0d755a30e134d5aada898111472ae7dd3f6512f8
EcaCosca <enriquecoscarelli@gmail.com>
2022-12-02 17:14:30 +0100

    Starting ex 3

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const Country = require('./models/countries')
+
+const app = express();
+const port = 3000
+
+app.use(express.json())
+
+mongoose.connect('mongodb+srv://new:newnewnew@sandbox.apxur.mongodb.net/?retryWrites=true&w=majority');
+
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, "MongoDB cannot connect:"))
+
+// 1. GET /api/countries
+app.get('/api/countries', (req, res) => {
+    Country.find({}, (err, data) => res.send(data));
+});
+
+// 2. POST /api/countries
+app.post('/api/countries', (req, res)=>{
+    Country.create(req.body)
+    .then(msg=>res.send(msg));
+})
+
+// 3. GET /api/countries/:code
+// This route should return a single country, based on the code provided. You should accept both an alpha 2 or an alpha 3 code. 
+
+
+
+
+app.listen(port, ()=>{
+    console.log(`Server is running on http://localhost:${port}/`)
+})
